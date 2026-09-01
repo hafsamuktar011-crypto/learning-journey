d4ff6e6e980ec6f10456f11b16c4ca948f6a68b7
hafsanuktar011-crypto <hafsamuktar011@gmail.com>
2026-09-02 02:05:09 +0300

    chore:initilize express

--- 0.2.AI-Powered Application Architecture & Building the Chat Bot/01.chatGPT Clone/Backend/index.js
+++ 0.2.AI-Powered Application Architecture & Building the Chat Bot/01.chatGPT Clone/Backend/index.js
@@ -1,8 +1,8 @@
 
-CREATE TABLE IF NOT EXISTS conversations{
-    id BIGINT UNSIGNED AUTO_INCREMENT PRIMARY KeyboardEvent,
-    role Enumerator('user','assistant') NOT null,
-    content TEXT NOT null,
+CREATE TABLE IF NOT EXISTS conversations (
+    id BIGINT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+    role ENUM('user', 'assistant') NOT NULL,
+    content TEXT NOT NULL,
     token_count INT UNSIGNED NOT NULL DEFAULT 0,
     created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
-}
+);
