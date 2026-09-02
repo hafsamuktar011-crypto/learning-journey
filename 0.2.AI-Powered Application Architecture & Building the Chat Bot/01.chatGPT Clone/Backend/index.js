5a04bfe986d051ec5fbd575a648c2581d0f71d86
hafsanuktar011-crypto <hafsamuktar011@gmail.com>
2026-09-02 13:17:54 +0300

    feat:add db folder

--- 0.2.AI-Powered Application Architecture & Building the Chat Bot/01.chatGPT Clone/Backend/index.js
+++ 0.2.AI-Powered Application Architecture & Building the Chat Bot/01.chatGPT Clone/Backend/index.js
@@ -1,8 +1,11 @@
 
-CREATE TABLE IF NOT EXISTS conversations (
-    id BIGINT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-    role ENUM('user', 'assistant') NOT NULL,
-    content TEXT NOT NULL,
-    token_count INT UNSIGNED NOT NULL DEFAULT 0,
-    created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
-);
+
+import express from "express";
+const app = express();
+
+
+
+
+app.listen(5500, () => {
+  console.log("Server is running at http://localhost:5500");
+});
