ae671ddf1c67a0d9e23400bbe7e0d9ab3b18967f
hafsanuktar011-crypto <hafsamuktar011@gmail.com>
2026-09-07 23:43:37 +0300

    fix: correct error handling in createConversation function

--- AI_Powered_FullStack/0.2.AI-Powered Application Architecture/01.chatGPT Clone/Backend/src/api/Chat/controller/chat.controller.js
+++ AI_Powered_FullStack/0.2.AI-Powered Application Architecture/01.chatGPT Clone/Backend/src/api/Chat/controller/chat.controller.js
@@ -1,11 +1,12 @@
 export function createConversation(req,res,next){
   try{
-
+  throw new Error('create conversation api error')
+   res.status(200).send('created convo')
   }
   catch(error){
 throw error
   }
-  res.status(200).send('created convo')
+ 
 }
 
 export function getConversation(req,res){
