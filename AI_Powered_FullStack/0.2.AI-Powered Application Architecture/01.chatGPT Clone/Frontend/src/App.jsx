f004c91a2d7da9cff42067fbed568c3a87e27f19
hafsanuktar011-crypto <hafsamuktar011@gmail.com>
2026-09-09 22:13:42 +0300

    feat: update lucide dependency to lucide-react and integrate Sidebar component in App

--- AI_Powered_FullStack/0.2.AI-Powered Application Architecture/01.chatGPT Clone/Frontend/src/App.jsx
+++ AI_Powered_FullStack/0.2.AI-Powered Application Architecture/01.chatGPT Clone/Frontend/src/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 
+import Sidebar from './Components/Sidebar/Sidebar'
 function App() {
   return (
     <div className='app'>
 
       {/* side bar */}
+      <Sidebar/>
       <main>
         {/* chatHeader */}
 
